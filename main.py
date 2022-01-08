5f258b85b1c3260cf4dfb40cf5e24d93bb8ed01c
Mirko Tritella <mirkotritella1999@gmail.com>
2022-01-08 15:32:36 +0100

    Added plotly visualizations

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from mimetypes import read_mime_types
 import streamlit as st
 import pandas as pd
-import datetime
-
+import plotter
 st.set_page_config(
      page_title="Covid-19 Roseto",
-     page_icon="🦠",
+     page_icon="favicon.ico",
      layout="wide",
      initial_sidebar_state="expanded",
      menu_items={
          'Get Help': 'https://github.com/Emeierkeio/roseto-covid/issues',
          'Report a bug': "https://github.com/Emeierkeio/roseto-covid/issues",
          'About': '''The Dashboard was built by [Mirko Tritella](https://www.github.com/Emeierkeio). It uses the data published daily on Facebook page [Città di Roseto degli Abruzzi](https://www.facebook.com/cittadiroseto).
-'''
+''',
      }
  )
 
@@ -22,15 +21,10 @@ def load_data():
     data.data = pd.to_datetime(data.data, format='%Y-%m-%d')
     # Handle NaN values
     data = data.interpolate(method='bfill', limit_direction='backward', axis=0)
-    data = data.set_index('data')
     return data
 
+
 raw_data = load_data()
-
-st.title('COVID-19 a Roseto degli Abruzzi (TE)')
-st.subheader('Dati aggiornati al ' + str(raw_data.index[-1])[:10])
-
-
 
 twoDaysMeasures = raw_data.tail(2)
 threeDaysMeasures = raw_data.tail(3)
@@ -39,33 +33,36 @@ oggi = twoDaysMeasures.tail(1)
 ieri = twoDaysMeasures.head(1)
 ieriancora = threeDaysMeasures.head(1)
 
+header = st.container()
+metrics = st.container()
+graph = st.container()
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Attualmente Positivi", int(oggi.attualmente_positivi.values[0]), int(oggi.attualmente_positivi.values[0] - ieri.attualmente_positivi.values[0]), delta_color="inverse")
-col2.metric("Ricoverati", int(oggi.ricoverati.values[0]), int(oggi.ricoverati.values[0] - ieri.ricoverati.values[0]), delta_color="inverse")
-col3.metric("Fine Sorveglianza", int(oggi.fine_sorveglianza.values[0]))
-
-
-col_1, col_2 = st.columns(2)
-if col_1.checkbox('Mostra dati'):
-    st.dataframe(raw_data)
-    
-with open('roseto.csv') as f:
-    col_2.download_button('Scarica dati in CSV', f)
-    
+with header:
+    st.title('COVID-19 a Roseto degli Abruzzi (TE)')
+    st.subheader('Dati aggiornati al ' + str(raw_data.index[-1])[:10])
 
 
-days = st.slider('Scegli il numero di giorni di cui vuoi visualizzare i dati', 0, len(raw_data.index), 120)
+with metrics:
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Attualmente Positivi", int(oggi.attualmente_positivi.values[0]), int(oggi.attualmente_positivi.values[0] - ieri.attualmente_positivi.values[0]), delta_color="inverse")
+    col2.metric("Ricoverati", int(oggi.ricoverati.values[0]), int(oggi.ricoverati.values[0] - ieri.ricoverati.values[0]), delta_color="inverse")
+    col3.metric("Fine Sorveglianza", int(oggi.fine_sorveglianza.values[0]))
 
-st.subheader('Attualmente Positivi')
-st.caption('Indicato, giornalmente, il numero totale di positivi.')
-st.area_chart(raw_data.tail(days).attualmente_positivi)
 
-st.subheader('Nuovi positivi giornalieri')
-st.caption('Indicato, giornalmente, il numero di nuovi positivi.')
-st.bar_chart(raw_data.tail(days).nuovi_positivi)
+    col_1, col_2 = st.columns(2)
+    if col_1.checkbox('Mostra dati'):
+        st.dataframe(raw_data)
+        
+    with open('roseto.csv') as f:
+        col_2.download_button('Scarica dati in CSV', f)
+        
 
-st.subheader('Numero di ricoverati')
-st.caption('Indicato, giornalmente, il numero di persone attualmente ricoverate residenti nel comune di Roseto degli Abruzzi.')
-st.area_chart(raw_data.tail(days).ricoverati)
+
+
+
+with graph:
+    days = st.slider('Scegli il numero di giorni di cui vuoi visualizzare i dati', 0, len(raw_data.index), 120)
+    st.plotly_chart(plotter.plot_daily_cases(raw_data.tail(days), 'attualmente_positivi'), use_container_width=True)
+    st.plotly_chart(plotter.plot_daily_cases(raw_data.tail(days), 'nuovi_positivi'), use_container_width=True)
+    st.plotly_chart(plotter.plot_daily_cases(raw_data.tail(days), 'ricoverati'), use_container_width=True)
 
---
+++ plotter.py
@@ -0,0 +1,35 @@
+import plotly.express as px
+import pandas as pd
+import streamlit as st
+
+# Open roseto.csv and plot the daily cases
+def main():
+    df = pd.read_csv('roseto.csv')
+    df.data = pd.to_datetime(df.data, format='%Y-%m-%d')
+    plot_daily_cases(df)
+
+
+def plot_daily_cases(df, measure):
+    fig = px.area(df, x = 'data', y= measure, title='Attualmente Positivi')
+
+
+    fig.update_layout(
+        legend=dict(
+                orientation="h",
+                yanchor="top",
+                y=-0.3,
+                xanchor="center",
+                x=0.5
+        ),
+        margin=dict(
+            l=0,
+            r=0,
+            t=0,
+            b=0
+        ),
+        plot_bgcolor='rgba(0,0,0,0)',
+    )
+    return fig
+
+if __name__ == '__main__':
+    main()
